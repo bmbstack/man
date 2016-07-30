06658189c01d54b522fd396554e2ef1cd7def45f
nixon <xiaoyao.ning@gmail.com>
2016-07-30 19:12:17 +0800

    完成数据读取和解析

--- router/vim.js
+++ router/vim.js
@@ -8,6 +8,19 @@ router.get('/vim', (req, res) => {
     fetch(VIM_RAW_URL)
         .then((response) => response.text())
         .then((text) => {
+            let lines = text.split('\n');
+            let titleRegexp = /^"=+begin\((.+)\)=+"$/igm;
+            let contentRegexp = /^.*(?:[inv]?(?:nore)?map)\s(.+)\s([^"]+)(?:"(.+))?$/igm;
+
+            lines.map((line) => {
+                if (titleRegexp.test(line)) {
+                    console.log(line)
+                }
+                if (contentRegexp.test(line)) {
+                    console.log(line)
+                }
+            });
+            
             res.render('man');
         });
 });
